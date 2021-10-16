79ff48915dad98b882dc79ece8a78c4b4c488d69
Anton Franzluebbers <anton@uga.edu>
2021-10-15 23:14:35 -0400

    add api spec, add fetching of headset details, improve website flow

--- velconnect/routes/api.py
+++ velconnect/routes/api.py
@@ -1,12 +1,27 @@
+from flask.helpers import send_from_directory
 from velconnect.auth import require_api_key
 from velconnect.db import connectToDB
 from velconnect.logger import logger
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, render_template, url_for
 import time
 import simplejson as json
 from random import random
 
 bp = Blueprint('api', __name__)
+
+
+
+@bp.route('/', methods=['GET'])
+@require_api_key(0)
+def api_home():
+    return render_template('api_spec.html')
+
+
+@bp.route('/api_spec.json', methods=['GET'])
+@require_api_key(0)
+def api_spec():
+    return send_from_directory('static', 'api_spec.json')
+
 
 
 @bp.route('/get_all_headsets', methods=['GET'])
@@ -48,8 +63,6 @@ def update_paring_code():
         return 'Must supply hw_id', 400
     if 'pairing_code' not in data:
         return 'Must supply pairing_code', 400
-    if 'pairing_code' not in data:
-        return 'Must supply pairing_code', 400
 
     conn, curr = connectToDB()
 
@@ -73,6 +86,24 @@ def update_paring_code():
 
     return 'Success'
 
+    
+
+@bp.route('/get_headset_details/<hw_id>', methods=['GET'])
+@require_api_key(10)
+def get_headset_details(hw_id):
+    return jsonify(get_headset_details_db(hw_id))
+
+
+def get_headset_details_db(hw_id):
+    conn, curr = connectToDB()
+    query = """
+    SELECT * FROM `Headset` WHERE hw_id=%(hw_id)s;
+    """
+    curr.execute(query, {'hw_id': hw_id})
+    values = [dict(row) for row in curr.fetchall()]
+    curr.close()
+    return jsonify(values)
+
 
 @bp.route('/get_room_details/<room_id>', methods=['GET'])
 @require_api_key(10)
@@ -91,13 +122,13 @@ def get_room_details_db(room_id):
     return jsonify(values)
 
 
-@bp.route('/create_room', methods=['GET'])
+@bp.route('/update_room/<room_id>', methods=['POST'])
 @require_api_key(10)
-def create_room():
-    return jsonify(create_room_db())
+def update_room(room_id):
+    return jsonify(update_room_db(room_id, request.json))
 
 
-def create_room_db():
+def update_room_db(room_id, data):
     room_id = random.randint(0, 9999)
     conn, curr = connectToDB()
     query = """
---
+++ velconnect/static/js/util.js
@@ -0,0 +1,31 @@
+
+function httpGetAsync(theUrl, callback, failCallback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4) {
+            if (xmlHttp.status == 200) {
+                callback(xmlHttp.responseText);
+            } else {
+                failCallback(xmlHttp.status);
+            }
+        }
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
