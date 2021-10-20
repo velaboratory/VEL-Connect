68e84b07c6a94e278c9a9123d10d052e6a7b897a
Anton Franzluebbers <anton@uga.edu>
2021-10-19 23:47:25 -0400

    set cookie properly

--- velconnect/static/js/util.js
+++ velconnect/static/js/util.js
@@ -31,6 +31,14 @@ function httpPostAsync(theUrl, data, callback, failCallback) {
     xmlHttp.send(JSON.stringify(data));
 }
 
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
