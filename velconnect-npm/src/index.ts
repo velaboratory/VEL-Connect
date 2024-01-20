00227e9b79f04898bb1e9f90769db8d5c0d243af
Anton Franzluebbers <anton@uga.edu>
2024-01-19 19:42:37 -0500

    generated types, lots of simplification

--- velconnect-npm/src/index.ts
+++ velconnect-npm/src/index.ts
@@ -1,272 +1,76 @@
 import PocketBase from "pocketbase";
-import type { Record } from "pocketbase";
+import { TypedPocketBase } from "./types/pocketbase-types";
 
+type VelConnectOptions = {
+  url?: string;
+  debugLog?: boolean;
+};
 
-export interface Device extends Record {
-  os_info: string;
-  friendly_name: string;
-  current_room: string;
-  current_app: string;
-  pairing_code: string;
-  data: string;
-  expand: { data?: DataBlock };
-}
-export interface DataBlock extends Record {
-  block_id: string;
-  owner: string;
-  data: { [key: string]: string };
-}
+export let pb: TypedPocketBase = new PocketBase(
+  "https://velconnect-v4.ugavel.com"
+) as TypedPocketBase;
+let debugLog = false;
+let initialized = false;
 
-export class VELConnect {
-
-  pb: PocketBase;
-  debugLog = false;
-
-  constructor() {
-    this.pb = new PocketBase();
-
-    this.pb.authStore.onChange((auth) => {
-      console.log("authStore changed", auth);
-      currentUser.set(pb.authStore.model);
-      if (pb.authStore.isValid) {
-      }
-    });
+export function initVelConnect(options: VelConnectOptions = {}) {
+  if (options.debugLog) {
+    debugLog = true;
   }
 
-
-  export const currentUser = writable(pb.authStore.model);
-
-
-
-  export const pairedDevices = writable<string[]>([]);
-  export const currentDeviceId = writable("");
-
-
-// const device = get(currentDevice);
-// if (device == '' && device.length > 0) {
-// 	currentDevice.set(device[0]);
-// }
-
-let unsubscribeDeviceFields: () => void;
-let unsubscribeDeviceData: () => void;
-let unsubscribeRoomData: () => void;
-let unsubscribeCurrentDevice: () => void;
-let unsubscribeCurrentUser: () => void;
-
-export let deviceFields = writable<Device | null>(null);
-export let deviceData = writable<DataBlock | null>(null);
-export let roomData = writable<DataBlock | null>(null);
-
-export let sending = false;
-
-export async function startListening(baseUrl: string) {
-  pb.baseUrl = baseUrl;
-  if (get(currentDeviceId) != "") {
-    const d = (await pb.collection("Device").getOne(get(currentDeviceId), {
-      expand: "data",
-    })) as Device;
-    deviceData.set(d.expand.data as DataBlock);
-    // we don't need expand anymore, since it doesn't work in subscribe()
-    d.expand = {};
-    deviceFields.set(d);
+  if (!initialized) {
+    pb = new PocketBase(
+      options.url ? options.url : "https://velconnect-v4.ugavel.com"
+    ) as TypedPocketBase;
+    log(`Initialized velconnect on ${pb.baseUrl}`);
   }
-
-  unsubscribeCurrentDevice = currentDeviceId.subscribe(async (val) => {
-    log("currentDeviceId subscribe change event");
-    unsubscribeDeviceFields?.();
-    unsubscribeDeviceData?.();
-    if (val != "") {
-      const d = (await pb
-        .collection("Device")
-        .getOne(get(currentDeviceId), { expand: "data" })) as Device;
-      deviceData.set(d.expand.data as DataBlock);
-      // we don't need expand anymore, since it doesn't work in subscribe()
-      d.expand = {};
-      deviceFields.set(d);
-
-      unsubscribeDeviceData = await pb
-        .collection("DataBlock")
-        .subscribe(d.data, async (data) => {
-          log("deviceData subscribe change event");
-          deviceData.set(data.record as DataBlock);
-        });
-
-      unsubscribeDeviceFields = await pb
-        .collection("Device")
-        .subscribe(val, async (data) => {
-          log("deviceFields subscribe change event");
-          const d = data.record as Device;
-          deviceFields.set(d);
-
-          // if the devie changes, the devicedata could change, so we need to resubscribe
-          unsubscribeDeviceData?.();
-          unsubscribeDeviceData = await pb
-            .collection("DataBlock")
-            .subscribe(d.data, async (data) => {
-              log("deviceData subscribe change event");
-              deviceData.set(data.record as DataBlock);
-            });
-
-          getRoomData(d);
-        });
-
-      if (d != null) getRoomData(d);
-    } else {
-      deviceFields.set(null);
-      deviceData.set(null);
-      roomData.set(null);
-    }
-  });
-
-  unsubscribeCurrentUser = currentUser.subscribe((user) => {
-    log(`currentUser changed ${user}`);
-    pairedDevices.set(user?.["devices"] ?? []);
-    currentDeviceId.set(get(pairedDevices)[0] ?? "");
-  });
+  // pb.authStore.onChange((auth) => {
+  //   console.log("authStore changed", auth);
+  // });
+  initialized = true;
 }
 
-export function stopListening() {
-  unsubscribeCurrentDevice?.();
-  unsubscribeDeviceFields?.();
-  unsubscribeDeviceData?.();
-  unsubscribeRoomData?.();
-  unsubscribeCurrentUser?.();
-  console.log("Stop listening");
+export function signOut() {
+  pb.authStore.clear();
 }
 
-async function getRoomData(device: Device) {
-  unsubscribeRoomData?.();
-
-  // create or just fetch room by name
-  let r: DataBlock | null = null;
+export async function pair(pairingCode: string) {
   try {
-    r = (await pb
-      .collection("DataBlock")
-      .getFirstListItem(
-        `block_id="${device.current_app}_${device.current_room}"`
-      )) as DataBlock;
-  } catch (e: any) {
-    r = (await pb.collection("DataBlock").create({
-      block_id: `${device.current_app}_${device.current_room}`,
-      category: "room",
-      data: {},
-    })) as DataBlock;
-  }
-  roomData.set(r);
-  if (r != null) {
-    unsubscribeRoomData = await pb
-      .collection("DataBlock")
-      .subscribe(r.id, (data) => {
-        log("roomData subscribe change event");
-        roomData.set(data.record as DataBlock);
-      });
-  } else {
-    console.error("Failed to get or create room");
-  }
-}
+    log("Pairing...");
 
-let abortController = new AbortController();
-export function delayedSend() {
-  console.log("fn: delayedSend()");
-
-  // abort the previous send
-  abortController.abort();
-  const newAbortController = new AbortController();
-  abortController = newAbortController;
-  setTimeout(() => {
-    if (!newAbortController.signal.aborted) {
-      send();
-    } else {
-      console.log("aborted");
-    }
-  }, 1000);
-}
-
-export function send() {
-  console.log("sending...");
-  sending = true;
-  let promises: Promise<any>[] = [];
-  const device = get(deviceFields);
-  const data = get(deviceData);
-  const room = get(roomData);
-  // TODO send changes only
-  if (device) {
-    promises.push(pb.collection("Device").update(device.id, device));
-  }
-  if (data) {
-    promises.push(pb.collection("DataBlock").update(data.id, data));
-  }
-  if (room) {
-    promises.push(pb.collection("DataBlock").update(room.id, room));
-  }
-  Promise.all(promises).then(() => {
-    sending = false;
-  });
-}
-
-export function removeDevice(d: string) {
-  pairedDevices.set(get(pairedDevices).filter((i) => i != d));
-
-  if (get(currentDeviceId) == d) {
-    console.log("Removed current device");
-
-    // if there are still devices left
-    if (get(pairedDevices).length > 0) {
-      currentDeviceId.set(get(pairedDevices)[0] ?? "");
-    } else {
-      currentDeviceId.set("");
-    }
-  }
-
-  const user = get(currentUser);
-  if (user) {
-    user["devices"] = user["devices"].filter((i: string) => i != d);
-    pb.collection("Users").update(user.id, user);
-  }
-}
-
-async function pair(pairingCode: string) {
-  try {
     // find the device by pairing code
-    const device = (await pb
+    const device = await pb
       .collection("Device")
-      .getFirstListItem(`pairing_code="${pairingCode}"`)) as Device;
-
-    // add it to the local data
-    currentDeviceId.set(device.id);
-    if (!get(pairedDevices).includes(device.id)) {
-      pairedDevices.set([...get(pairedDevices), device.id]);
-    }
+      .getFirstListItem(`pairing_code="${pairingCode}"`);
 
     // add it to my account if logged in
-    const u = get(currentUser);
+    const u = pb.authStore.model;
     if (u) {
       // add the device to the user's devices
       u["devices"].push(device.id);
 
       // add the account data to the device
       if (
-        u.user_data == null ||
-        u.user_data == undefined ||
-        u.user_data == ""
+        u["user_data"] == null ||
+        u["user_data"] == undefined ||
+        u["user_data"] == ""
       ) {
         // create a new user data block if it doesn't exist on the user already
         const userDataBlock = await pb.collection("DataBlock").create({
           category: "device",
           data: {},
-          owner: u.id,
+          owner: u["id"],
         });
-        u.user_data = userDataBlock.id;
+        u["user_data"] = userDataBlock.id;
       }
-      device["data"] = u.user_data;
-      device["owner"] = u.id;
-      device["past_owners"] = [...device["past_owners"], u.id];
+      device["data"] = u["user_data"];
+      device["owner"] = u["id"];
+      device["past_owners"] = [...device["past_owners"], u["id"]];
 
       await pb.collection("Device").update(device.id, device);
-      await pb.collection("Users").update(u.id, u);
+      await pb.collection("Users").update(u["id"], u);
     }
 
-    return { error: null };
+    return { error: null, deviceId: device.id };
   } catch (e) {
     console.error("Not found: " + e);
     if (e == "ClientResponseError 404: The requested resource wasn't found.") {
@@ -282,10 +86,12 @@ async function pair(pairingCode: string) {
 
 export async function login(username: string, password: string) {
   try {
-    await pb.collection("Users").authWithPassword(username, password);
-    return {};
-  } catch (err: any) {
-    return err;
+    const ret = await pb
+      .collection("Users")
+      .authWithPassword(username, password);
+    return { ret };
+  } catch (error: any) {
+    return { error };
   }
 }
 
@@ -303,15 +109,8 @@ export async function signUp(username: string, password: string) {
   }
 }
 
-export function signOut() {
-  pb.authStore.clear();
-}
-
-function log(msg: string) {
+function log(msg: any) {
   if (debugLog) {
     console.log(msg);
   }
-}
-
-  
 }
---
+++ velconnect-npm/src/types/pocketbase-types.ts
@@ -0,0 +1,106 @@
+/**
+* This file was @generated using pocketbase-typegen
+*/
+
+import type PocketBase from 'pocketbase'
+import type { RecordService } from 'pocketbase'
+
+export enum Collections {
+	DataBlock = "DataBlock",
+	Device = "Device",
+	UserCount = "UserCount",
+	Users = "Users",
+}
+
+// Alias types for improved usability
+export type IsoDateString = string
+export type RecordIdString = string
+export type HTMLString = string
+
+// System fields
+export type BaseSystemFields<T = never> = {
+	id: RecordIdString
+	created: IsoDateString
+	updated: IsoDateString
+	collectionId: string
+	collectionName: Collections
+	expand?: T
+}
+
+export type AuthSystemFields<T = never> = {
+	email: string
+	emailVisibility: boolean
+	username: string
+	verified: boolean
+} & BaseSystemFields<T>
+
+// Record types for each collection
+
+export type DataBlockRecord<Tdata = unknown> = {
+	block_id?: string
+	category?: string
+	data?: null | Tdata
+	modified_by?: string
+	owner?: RecordIdString
+}
+
+export type DeviceRecord = {
+	current_app?: string
+	current_room?: string
+	data: RecordIdString
+	friendly_name?: string
+	last_online?: IsoDateString
+	modified_by?: string
+	os_info?: string
+	owner?: RecordIdString
+	pairing_code?: string
+	past_owners?: RecordIdString[]
+}
+
+export type UserCountRecord = {
+	app_id?: string
+	platform?: string
+	room_id?: string
+	room_users?: number
+	total_users?: number
+	version?: string
+}
+
+export type UsersRecord = {
+	avatar?: string
+	devices?: RecordIdString[]
+	name?: string
+	profiles?: RecordIdString[]
+}
+
+// Response types include system fields and match responses from the PocketBase API
+export type DataBlockResponse<Tdata = unknown, Texpand = unknown> = Required<DataBlockRecord<Tdata>> & BaseSystemFields<Texpand>
+export type DeviceResponse<Texpand = unknown> = Required<DeviceRecord> & BaseSystemFields<Texpand>
+export type UserCountResponse<Texpand = unknown> = Required<UserCountRecord> & BaseSystemFields<Texpand>
+export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>
+
+// Types containing all Records and Responses, useful for creating typing helper functions
+
+export type CollectionRecords = {
+	DataBlock: DataBlockRecord
+	Device: DeviceRecord
+	UserCount: UserCountRecord
+	Users: UsersRecord
+}
+
+export type CollectionResponses = {
+	DataBlock: DataBlockResponse
+	Device: DeviceResponse
+	UserCount: UserCountResponse
+	Users: UsersResponse
+}
+
+// Type for usage with type asserted PocketBase instance
+// https://github.com/pocketbase/js-sdk#specify-typescript-definitions
+
+export type TypedPocketBase = PocketBase & {
+	collection(idOrName: 'DataBlock'): RecordService<DataBlockResponse>
+	collection(idOrName: 'Device'): RecordService<DeviceResponse>
+	collection(idOrName: 'UserCount'): RecordService<UserCountResponse>
+	collection(idOrName: 'Users'): RecordService<UsersResponse>
+}
